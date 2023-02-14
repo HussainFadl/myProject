0c004b6fb6315341afade92e6be2d408859c11bc
Hussain <husfadl2000@hotmail.com>
2023-02-14 18:32:04 +0200

    Creating some changes on index.html by using app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Hello world");
+alert("Hello World!\nThis is  my first JavaScript prograam\n"+
+"أنا اسمي حسين محمد سيد فضل الله\n"+
+"أعمل مهندس زراعي سابق و أعمل حاليا مطور \n"+
+"تطبيقات هواتف ذكية") ;
